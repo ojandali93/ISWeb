84d99145864590d8348f56cc8f118478327e535e
omar jandali <omarjandali93@gmail.com>
2024-05-08 10:25:00 -0700

    Omar Jandali. prevented search bar refresh

--- src/Components/Inputs/SearchComponent.tsx
+++ src/Components/Inputs/SearchComponent.tsx
@@ -10,10 +10,15 @@ interface ComponentProps {
 }
 
 const SearchComponent: React.FC<ComponentProps>  = ({searchTerm, handler, activeSearch, handleActiveSearch, placeholder}) => {
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='rounded-md flex flex-row items-center m-2 w-full'>
       <Search size={24} color='white'/>
-      <form className='flex-1 mx-1'>
+      <form className='flex-1 mx-1' onSubmit={handleSubmit}>
         <input 
           value={searchTerm}
           onChange={(e) => {handler(e.target.value)}}
