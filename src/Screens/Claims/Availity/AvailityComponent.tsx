cc7c5aaef7aa140cebee34ffbc96050e729ee843
Brian Jaber <brianjaber@gmail.com>
2024-05-09 12:57:58 -0700

    Brian Jaber: Fixes issue where Availity Table headers were every other row.

--- src/Screens/Claims/Availity/AvailityComponent.tsx
+++ src/Screens/Claims/Availity/AvailityComponent.tsx
@@ -8,9 +8,7 @@ const AvailityComponent = () => {
   // try calling useEffect here after routing to see if this will work for us.
   return (
     <div>
-      {availityData.map( (record: any, index: any) => (
-      <TableComponent key={index} records={[record]} columns={AvailityOptions} users={allUsers} table="Availity"/>
-      ))}
+      <TableComponent records={availityData} columns={AvailityOptions} users={allUsers} table="Availity"/>
     </div>
   )
 }
